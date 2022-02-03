c6841a1d69553c2e9118463d7c49130dbaf30d2a
Bradley Miller <bradley.miller@bsg.co.za>
2022-02-03 14:03:03 +0200

    Module 2 Hydrate the state complete

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -2,10 +2,23 @@ import React from 'react'
 
 import './Tile.css'
 
-const Tile = () => {
 
+const Tile = ({ selected, matched, color, svg }) => {
+
+  const tileColor = { backgroundColor: selected || matched ? color : null };
+
+  //const TileIcon = matched || selected ? svg : null;
+  const TileIcon = svg;
+
+  //const tileColor = 'red'
   return (
-    <div className='Tile'>
+    // <div className='Tile' style={{ backgroundColor: tileColor.backgroundColor }}>
+    <div className='Tile' style={selected || matched ? { backgroundColor: color } : null}>
+      {
+        matched || selected
+          ? <TileIcon />
+          : null
+      }
     </div>
   )
 }
